eeec54d7db6481695f9e6c5b26d868fd6226cf41
kaedeshimizu <kaedeshimizu@163.com>
2023-07-25 16:13:18 +0800

    提交

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # 因为现在有多个版本的程序，所以在这里进行一下分流
 # 目前推荐使用jitsu版本
 import main_jitsu
-import uvicorn
-
-uvicorn.run(main_jitsu.app, host="127.0.0.1", port=8000)
+from fastapi import FastAPI
+if __name__ == "__main__":
+    import uvicorn
+    
+    uvicorn.run(main_jitsu.app, host="127.0.0.1", port=8000)
